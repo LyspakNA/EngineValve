d38185b8fbfc98ba0c67dacc53ab8e610e6ad794
Nikolay Lyspak <niko.lyspak16@yandex.ru>
2021-12-27 18:27:51 +0700

    Добавлены Unit-тесты

---
+++ EngineValve/EngineValve.Tests/ParameterTest.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EngineValveParameters;
+using NUnit.Framework;
+
+namespace EngineValve.Tests
+{
+	[TestFixture]
+	public class ParameterTests
+	{
+		/// <summary>
+		/// Объект шаблонного класса для тестов
+		/// </summary>
+		private Parameter<double> _testParameter
+			= new Parameter<double>("Test parameter", 100, 0, 50);
+
+		[TestCase("", Description = "Пустое значение имени")]
+		[TestCase(" ", Description = "Имя состоящие из разделителя")]
+		[Test(Description = "Негативный тест на сеттер имени")]
+		public void TestParameterSet_NameUncorrect(string wrongName)
+		{
+			Assert.Throws<Exception>(()=>_testParameter.Name = wrongName, 
+				"Возникает, если имя пустое или состоит из пробела");
+		}
+
+		[TestCase(-1, Description = "Значение максимума меньше минимума")]
+		[Test(Description = "Негативный тест на сеттер максимума")]
+		public void TestParameterSet_MaxUncorrect(double wrongMax)
+		{
+			Assert.Throws<Exception>(() => _testParameter.Max = wrongMax,
+				"Возникает, если максимальное значение меньше минимального");
+		}
+
+		[TestCase(-1, Description = "Значение меньше допустимого")]
+		[TestCase(101, Description = "Значение больше допустимого")]
+		[Test(Description = "Негативный тест на сеттер параметра")]
+		public void TestParameterSet_ValueUncorrect(double wrongValue)
+		{
+			Assert.Throws<Exception>(() => _testParameter.Value = wrongValue,
+				"Возникает, если высота крепления больше 100 или меньше 0");
+		}
+	}
+}
