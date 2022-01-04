6347f55a3e84992db9a507548ede7406aeaf28fd
Nikolay Lyspak <niko.lyspak16@yandex.ru>
2022-01-04 20:39:33 +0700

    Дописана пояснительная записка

--- EngineValve/EngineValve/MainForm.cs
+++ EngineValve/EngineValve/MainForm.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
---
+++ EngineValve/EngineValveBuilderTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using EngineValveParameters;
+using EngineValveBuild;
+using Microsoft.VisualBasic.Devices;
+
+namespace EngineValveBuilderTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var parameters = new EngineValveParameterCollection();
+			var builder = new EngineValveBuilder(parameters);
+			var stopwatch = new Stopwatch();
+			stopwatch.Start();
+			var streamWriter = new StreamWriter($"logapiService.txt", true);
+			Process currentProcess = System.Diagnostics.Process.GetCurrentProcess();
+			var count = 0;
+			while (true)
+			{
+				builder.BuildEngineValve();
+				var computerInfo = new ComputerInfo();
+			}
+		}
+	}
+}
